5b37368643dc74f86b460cb503e97bee048be1e0
Vishwas Gowda <vishwas.g217@gmail.com>
2023-09-12 22:48:10 +0530

    latest news feature

--- src/news_sentiment.py
+++ src/news_sentiment.py
@@ -13,32 +13,59 @@ import json
 AV_API_KEY = st.secrets["av_api_key"]
 symbol = "MSFT"
 
-current_datetime = datetime.now()
-one_year_ago = current_datetime - timedelta(days=365)
-formatted_time_from = one_year_ago.strftime("%Y%m%dT%H%M")
-print("time_from=", formatted_time_from)
+
+def news_sentiment(symbol):
+
+    current_datetime = datetime.now()
+    one_year_ago = current_datetime - timedelta(days=365)
+    formatted_time_from = one_year_ago.strftime("%Y%m%dT%H%M")
+    print("time_from=", formatted_time_from)
 
 
 
 
-url = "https://www.alphavantage.co/query"
-params = {
-    "function": "NEWS_SENTIMENT",
-    "tickers": symbol,
-    "apikey": AV_API_KEY,
-    "sort": "RELEVANCE",
-    "limit": "5"
-}
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function": "NEWS_SENTIMENT",
+        "tickers": symbol,
+        "apikey": AV_API_KEY,
+        "sort": "LATEST",
+    }
 
-# Send a GET request to the API
-response = requests.get(url, params=params)
-if response.status_code == 200:
-    data = response.json()
-    json_object = json.dumps(data)
-    with open("sample.json", "w") as f:
-        f.write(json_object)
-    
-else:
-    print(f"Error: {response.status_code} - {response.text}")
+    # Send a GET request to the API
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
 
-print(len(data["feed"]))
+        news = []
+
+        for i in data["feed"][:10]:
+            temp = {}
+            temp["title"] = i["title"]
+            temp["url"] = i["url"]
+            temp["authors"] = i["authors"]
+            temp["source"] = i["source_domain"]
+            topics = []
+            for j in i["topics"]:
+                topics.append(j["topic"])
+            temp["topics"] = topics
+            sentiment_score = ""
+            for j in i["ticker_sentiment"]:
+                if j["ticker"] == symbol:
+                    sentiment_score = j["ticker_sentiment_score"]
+                    break
+            temp["sentiment_score"] = sentiment_score
+
+            news.append(temp)
+
+        
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+    return news
+
+if __name__ == "__main__":
+    news = news_sentiment("AAPL")
+    for i in news:
+        print(i)
+
